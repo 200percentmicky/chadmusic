1bbbc1b486a01e25257ba9645373a043801072ff
mickykuna <iiz10ninja@gmail.com>
2020-11-29 17:13:01 -0600

    💥 Handling errors.

--- src/listeners/playerError.js
+++ src/listeners/playerError.js
@@ -17,6 +17,7 @@ module.exports = class ListenerPlayerError extends Listener
         if (errsplit.includes('result!')) return message.error(`No results found for \`${args.slice(1).join(' ')}\`.`, 'Track Error');
         if (errsplit.includes('Unsupported')) return message.error('That URL is not supported.', 'Track Error');
         if (errsplit.includes('416')) return message.error('Received \`Status code: 416 [Range Not Satisfiable]\`. This is a weird response. Please try again.', 'Track Error');
+        if (errsplit.includes('format')) return message.error('Cannot find a format of the video to play. The owner of the video has disabled playback on other websites, or the video is unavailable.', 'Track Error');
         if (error.name.includes('Error [VOICE_CONNECTION_TIMEOUT]')) return message.error('The connection was not established within 15 seconds. Please try again later.', 'Voice Connection Timeout')
         message.error(error.message, `Track Error \`${error.name}\``);
         message.recordError('error', 'None', 'Player Error', error.stack);
