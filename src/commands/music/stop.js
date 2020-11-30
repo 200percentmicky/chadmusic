2fe70248d67e3724919a1966c686c4f4c96f9985
mickykuna <iiz10ninja@gmail.com>
2020-11-29 22:51:31 -0600

    stop has dj limits. changes in summon.

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -31,8 +31,18 @@ module.exports = class CommandStop extends Command
         if (!this.client.player.isPlaying(message) || !currentVc) return message.warn('Nothing is currently playing in this server.');
         else if (vc.id !== currentVc.channel.id) return message.error('You must be in the same voice channel that I\'m in to use that command.');
 
-        this.client.player.stop(message);
-        message.member.voice.channel.leave();
-        return message.ok('Stopped the player and cleared the queue. ⏹');
+        const vc = message.member.voice.channel;
+        if (!vc) return message.error('You are not in a voice channel.');
+
+        const currentVc = this.client.voice.connections.get(message.guild.id);
+        if (currentVc.channel.members.size <= 2 || dj)
+        {
+            this.client.player.stop(message);
+            message.member.voice.channel.leave();
+            return message.ok('Stopped the player and cleared the queue. ⏹');
+        } else {
+            return message.error('You must have the DJ role on this server, or the **Manage Channel** permission to use that command. Being alone with me works too!');
+        }
+
     }
 };
--- src/commands/music/summon.js
+++ src/commands/music/summon.js
@@ -32,10 +32,17 @@ module.exports = class CommandSummon extends Command
         const permissions = vc.permissionsFor(this.client.user.id).has(['CONNECT', 'SPEAK']);
         if (!permissions) return message.error(`Missing **Connect** or **Speak** permissions for **${vc.name}**`);
 
-        if (this.client.voice.connections.get(message.guild.id)) return message.warn('I\'m already in a voice channel. Let\'s get this party started!');
+        const currentVc = this.client.voice.connections.get(message.guild.id)
+        if (currentVc)
+        {
+            if (vc.id !== currentVc.id) return message.error('I\'m currently binded to a different voice channel.');
+            else return message.warn('I\'m already in a voice channel. Let\'s get this party started!');
 
-        vc.join();
-        message.react('📥');
-        return message.ok(`Joined **${vc.name}**.`);
+        } else {
+            vc.join();
+            message.react('📥');
+            return message.ok(`Joined **${vc.name}**.`);
+        }
+        
     }
 };
