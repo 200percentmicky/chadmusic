261b3185734f36a7e8ccfc3e66fc673a7a8effd8
mickykuna <iiz10ninja@gmail.com>
2020-11-29 22:54:14 -0600

    🐛 Fixed a declaration error in stop

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -31,9 +31,6 @@ module.exports = class CommandStop extends Command
         if (!this.client.player.isPlaying(message) || !currentVc) return message.warn('Nothing is currently playing in this server.');
         else if (vc.id !== currentVc.channel.id) return message.error('You must be in the same voice channel that I\'m in to use that command.');
 
-        const vc = message.member.voice.channel;
-        if (!vc) return message.error('You are not in a voice channel.');
-
         const currentVc = this.client.voice.connections.get(message.guild.id);
         if (currentVc.channel.members.size <= 2 || dj)
         {
