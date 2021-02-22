e44297e531fa9fa8259ea9a7d74911605887a5d0
mickykuna <iiz10ninja@gmail.com>
2021-02-22 02:39:41 -0600

    Fixed lib issue. "playSong" would emit undefined.

--- src/listeners/playSong.js
+++ src/listeners/playSong.js
@@ -12,25 +12,27 @@ module.exports = class ListenerPlaySong extends Listener {
   }
 
   async exec (message, queue, song) {
+    const msg = queue.initMessage // message sometimes returns 'undefined'
+    const textChannel = msg.channel // Again, message sometimes returns 'undefined'.
+    const channel = queue.connection.channel // Same.
+    const guild = channel.guild // This as well...
+    // This be some weird shit above...
+
     if (queue.songs.length === 1) { // If someone started a new queue.
-      const settings = this.client.settings.get(message.guild.id)
-      const dj = message.member.roles.cache.has(settings.djRole) || message.channel.permissionsFor(message.member.user.id).has(['MANAGE_CHANNELS'])
+      const settings = this.client.settings.get(guild.id)
+      const dj = msg.member.roles.cache.has(settings.djRole) || channel.permissionsFor(msg.member.user.id).has(['MANAGE_CHANNELS'])
       if (settings.maxTime) {
         if (!dj) {
           if (parseInt(song.duration + '000') > settings.maxTime) { // DisTube omits the last three digits in the songs duration.
             // Stupid fix.
-            if (message.content.includes(this.client.prefix.getPrefix(message.guild.id) + ('skip' || 's'))) return
+            if (msg.content.includes(this.client.prefix.getPrefix(guild.id) + ('skip' || 's'))) return
             this.client.player.stop(message)
-            return message.say('no', `You cannot add this song to the queue since the duration of this song exceeds the max limit of \`${prettyms(settings.maxTime, { colonNotation: true })}\` for this server.`)
+            return msg.say('no', `You cannot add this song to the queue since the duration of this song exceeds the max limit of \`${prettyms(settings.maxTime, { colonNotation: true })}\` for this server.`)
           }
         }
       }
       message.say('ok', `Added **${song.name}** to the queue.`)
     }
-
-    const textChannel = queue.initMessage.channel // Because message sometimes returns 'undefined'.
-    const channel = queue.connection.channel // Same.
-    const guild = channel.guild // This as well...
 
     textChannel.send(new MessageEmbed()
       .setColor(this.client.utils.randColor())
