13ee59be711af561758f85b63c0a4751f03552a4
mickykuna <iiz10ninja@gmail.com>
2021-04-24 15:24:11 -0500

    New look when presenting fatal errors

--- src/commands/utilities/test.js
+++ src/commands/utilities/test.js
@@ -3,8 +3,8 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = class CommandTest extends Command {
   constructor () {
-    super('test', {
-      aliases: ['test'],
+    super('jstest', {
+      aliases: ['jstest'],
       category: '🛠 Utilities',
       description: {
         text: 'Test command. Does I do work????'
@@ -13,15 +13,32 @@ module.exports = class CommandTest extends Command {
   }
 
   async exec (message) {
-    const text = 'poggers'.repeat(500)
-    const arr = text.match(/.{1,2048}/g) // Build the array
+    const args = message.content.split(/ +/g)
 
-    for (const chunk of arr) { // Loop through every element
-      const embed = new MessageEmbed()
-        .setColor(this.client.utils.randColor())
-        .setDescription(chunk)
+    switch (args[1]) {
+      case 'error': {
+        const e = new Error('Successfully threw an error. How did I do? :3')
+        e.name = 'GuruMeditationTest'
+        throw e
+      }
 
-      await message.channel.send({ embed }) // Wait for the embed to be sent
+      case 'pog': {
+        const text = 'poggers'.repeat(500)
+        const arr = text.match(/.{1,2048}/g) // Build the array
+
+        for (const chunk of arr) { // Loop through every element
+          const embed = new MessageEmbed()
+            .setColor(this.client.utils.randColor())
+            .setDescription(chunk)
+
+          await message.channel.send({ embed }) // Wait for the embed to be sent
+        }
+        break
+      }
+
+      default: {
+        message.say('ok', 'Yay! I\'m working as I should! What was I suppose to do again? 😗')
+      }
     }
   }
 }
--- src/listeners/commandError.js
+++ src/listeners/commandError.js
@@ -1,4 +1,6 @@
 const { Listener } = require('discord-akairo')
+const { MessageEmbed } = require('discord.js')
+const { stripIndents } = require('common-tags')
 
 module.exports = class ListenerCommandError extends Listener {
   constructor () {
@@ -9,7 +11,25 @@ module.exports = class ListenerCommandError extends Listener {
   }
 
   async exec (error, message, command) {
-    message.channel.send(`💢 \`Bruh Moment in command '${command.id}'. An error report was sent to the bot owner.\``)
+    // Normally presented in an embed. I wanna make it look pretty!
+    const perms = message.channel.permissionsFor(this.client.user.id).has(['EMBED_LINKS'])
+    if (perms) {
+      const guru = new MessageEmbed()
+        .setColor(process.env.COLOR_ERROR)
+        .setTitle('🧘‍♀️ Guru Meditation')
+        .setDescription(`Something bad happened while executing \`${command.id}\`.\n\`\`\`js\n${error.name}: ${error.message}\`\`\``)
+        .setFooter('An error report was sent to the bot owner.')
+      message.reply({ embed: guru, allowedMentions: { repliedUser: false } })
+    } else {
+      const guru = stripIndents`
+      :woman_in_lotus_position: **Guru Meditation**
+      Something bad happened while executing \`${command.id}\`. An error report was sent to the bot owner.
+      \`\`\`js
+      ${error.name}: ${error.message}
+      \`\`\`
+      `
+      message.reply(guru, { allowedMentions: { repliedUser: false } })
+    }
     message.recordError('error', command, 'Command Error', error.stack)
   }
 }
