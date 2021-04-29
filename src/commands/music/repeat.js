b33048b30af826cf6c63015cfe9e2f716870bbac
mickykuna <iiz10ninja@gmail.com>
2021-04-29 15:09:45 -0500

    DJ permission check is applied.

--- src/commands/music/repeat.js
+++ src/commands/music/repeat.js
@@ -14,36 +14,54 @@ module.exports = class CommandRepeat extends Command {
   }
 
   async exec (message) {
+    const djMode = await this.client.djMode.get(message.guild.id)
+    const djRole = await this.client.djRole.get(message.guild.id)
+    const dj = message.member.roles.cache.has(djRole) || message.channel.permissionsFor(message.member.user.id).has(['MANAGE_CHANNELS'])
+    if (djMode) {
+      return message.say('no', 'DJ Mode is currently active. You must have the DJ Role or the **Manage Channels** permission to use music commands at this time.', 'DJ Mode')
+    }
+
     const args = message.content.split(/ +/g)
     const player = this.client.player
     const queue = player.getQueue(message)
 
-    switch (args[1]) {
-      case 'off': {
-        await player.setRepeatMode(message, 0)
-        message.say('ok', 'Repeat has been disabled.')
-        break
-      }
-      case 'song': {
-        await player.setRepeatMode(message, 1)
-        message.say('ok', 'Enabled repeat to **🔂 Repeat Song**')
-        break
-      }
-      case 'queue': {
-        await player.setRepeatMode(message, 2)
-        message.say('ok', 'Enabled repeat to **🔁 Repeat Queue**')
-        break
-      }
-      default: {
-        if (queue.repeatMode !== 0) {
+    const vc = message.member.voice.channel
+    if (!vc) return message.say('error', 'You are not in a voice channel.')
+
+    const currentVc = this.client.voice.connections.get(message.guild.id)
+
+    if (!this.client.player.isPlaying(message) || !currentVc) return message.say('warn', 'Nothing is currently playing in this server.')
+
+    if (currentVc.channel.members.size <= 2 || dj) {
+      switch (args[1]) {
+        case 'off': {
           await player.setRepeatMode(message, 0)
           message.say('ok', 'Repeat has been disabled.')
           break
         }
-        await player.setRepeatMode(message, 1)
-        message.say('ok', 'Enabled repeat to **🔂 Repeat Song**')
-        break
+        case 'song': {
+          await player.setRepeatMode(message, 1)
+          message.say('ok', 'Enabled repeat to **🔂 Repeat Song**')
+          break
+        }
+        case 'queue': {
+          await player.setRepeatMode(message, 2)
+          message.say('ok', 'Enabled repeat to **🔁 Repeat Queue**')
+          break
+        }
+        default: {
+          if (queue.repeatMode !== 0) {
+            await player.setRepeatMode(message, 0)
+            message.say('ok', 'Repeat has been disabled.')
+            break
+          }
+          await player.setRepeatMode(message, 1)
+          message.say('ok', 'Enabled repeat to **🔂 Repeat Song**')
+          break
+        }
       }
+    } else {
+      return message.say('error', 'You must have the DJ role on this server, or the **Manage Channel** permission to use that command. Being alone with me works too!')
     }
   }
 }
