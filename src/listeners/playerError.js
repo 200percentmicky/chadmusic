6019970986881e9ae35a5fb4a7abf519be89a80c
mickykuna <iiz10ninja@gmail.com>
2021-05-06 03:58:44 -0500

    Added a known error.

--- src/listeners/playerError.js
+++ src/listeners/playerError.js
@@ -15,6 +15,7 @@ module.exports = class ListenerPlayerError extends Listener {
     // TODO: These should be compressed into an object later.
     if (errsplit.includes('result!')) return message.say('error', `No results found for \`${args.slice(1).join(' ')}\`.`, 'Player Error')
     if (errsplit.includes('extract') || errsplit.includes('Unsupported')) return message.say('error', 'Not a supported URL or the URL is invalid.', 'Player Error')
+    if (errsplit.includes('403')) return message.say('error', 'URL returned HTTP 403 (Forbidden)')
     if (errsplit.includes('416')) return message.say('error', 'Received `Status code: 416 [Range Not Satisfiable]`. This is a weird response. Please try again.', 'Player Error')
     if (errsplit.includes('Cookie')) return message.say('error', 'Cookie header used in the request has expired. The bot owner has been notified about this.', 'Player Error')
     if (errsplit.includes('format')) return message.say('error', 'Cannot find a format of the video to play. The owner of the video has disabled playback on other websites, or the video is unavailable.', 'Player Error')
