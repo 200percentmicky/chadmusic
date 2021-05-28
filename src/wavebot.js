b015773abd2799e08687404456baae234c8ab3c4
mickykun-ar <iiz10ninja@gmail.com>
2021-05-28 01:14:26 -0500

    Redirecting Mongo URI

--- src/wavebot.js
+++ src/wavebot.js
@@ -60,7 +60,7 @@ const moment = require('moment')
 
 /* Mongoose */
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
