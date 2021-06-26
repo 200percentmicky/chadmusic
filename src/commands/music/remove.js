2154511073b9d01aee358b6c1a557aaac7d30790
mickykun-ar <iiz10ninja@gmail.com>
2021-06-26 18:16:27 -0500

    remove: multiple entries can be removed.

--- src/commands/music/remove.js
+++ src/commands/music/remove.js
@@ -1,14 +1,14 @@
 const { Command } = require('discord-akairo')
 
-module.exports = class CommandStop extends Command {
+module.exports = class CommandRemove extends Command {
   constructor () {
     super('remove', {
       aliases: ['remove', 'removesong'],
       category: '🎶 Music',
       description: {
-        text: 'Removes an entry from the queue.',
-        usage: '<int:queue_entry>',
-        details: '`<int:queue_entry>` The queue entry to remove from the queue.'
+        text: 'Removes an entry or multiple entries from the queue.',
+        usage: '<int:queue_entry/start> [int:end]',
+        details: '`<int:queue_entry/starting>` The queue entry to remove from the queue, or the starting position.\n[int:end] The end position for removing multiple entries.\nEvery entry from the starting to end position will be removed from the queue.'
       },
       channel: 'guild',
       clientPermissions: ['EMBED_LINKS']
@@ -31,13 +31,47 @@ module.exports = class CommandStop extends Command {
     if (!this.client.player.isPlaying(message) || !currentVc) return message.say('warn', 'Nothing is currently playing in this server.')
     else if (vc.id !== currentVc.channel.id) return message.say('error', 'You must be in the same voice channel that I\'m in to use that command.')
 
-    if (!args[1]) return message.usage('remove <int:queue_entry>')
+    if (!args[1]) return message.usage('remove <int:queue_entry/starting> [int:end]')
 
     if (currentVc.channel.members.size <= 2 || dj) {
       const queue = this.client.player.getQueue(message)
-      const song = queue.songs[args[1]]
-      queue.songs.splice(args[1], 1)
-      return message.say('ok', `Removed **${song.name}** from the queue.`)
+
+      /* Remove multiple entries from the queue. */
+      if (args[2]) {
+        /* Parsing arguments as numbers */
+        const start = parseInt(args[1])
+        const end = parseInt(args[2])
+
+        /* Checking if the arguments are numbers. */
+        if (isNaN(start)) return message.say('error', 'Starting position must be a number.')
+        if (isNaN(end)) return message.say('error', 'Ending position must be a number.')
+
+        /* Slice original array to get the length. */
+        const n = parseInt(queue.songs.slice(start, end).length + 1)
+
+        /* This command is for multiple entries. Tell the user to use the 'remove' command instead. */
+        if (n === 1) {
+          const prefix = this.client.settings.get(message.guild.id, 'prefix', process.env.PREFIX)
+          return message.say('warn', `This command is used to remove multiple entries. Use \`${prefix}remove\` if you want to remove just one entry from the queue.`)
+        }
+
+        /* Modify queue to remove the entries. */
+        queue.songs.splice(start, n)
+
+        return message.say('ok', `Removed **${n}** entries from the queue.`)
+      } else {
+        /* Removing only one entry from the queue. */
+        const song = queue.songs[args[1]]
+
+        /* Checking if argument is a number. */
+        const n = parseInt(args[1])
+        if (isNaN(n)) return message.say('error', 'Selection must be a number.')
+
+        /* Modify queue to remove the specified entry. */
+        queue.songs.splice(args[1], 1)
+
+        return message.say('ok', `Removed **${song.name}** from the queue.`)
+      }
     } else {
       return message.say('error', 'You must have the DJ role on this server, or the **Manage Channel** permission to use that command. Being alone with me works too!')
     }
