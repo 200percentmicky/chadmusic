2acf4e43df526b3dec1664f260872688611feb58
mickykun-ar <iiz10ninja@gmail.com>
2021-07-30 03:45:45 -0500

    Removing an outdated param

--- src/listeners/addList.js
+++ src/listeners/addList.js
@@ -8,7 +8,9 @@ module.exports = class ListenerAddList extends Listener {
     })
   }
 
-  async exec (message, queue, playlist) {
+  async exec (queue, playlist) {
+    const channel = queue.textChannel
+    const message = channel.messages.cache.find(msg => msg)
     this.client.ui.say(message, 'ok', `Added the playlist **${playlist.name}** with **${playlist.songs.length}** entr${playlist.songs.length === 1 ? 'y' : 'ies'} to the queue.`)
   }
 }
