d244ccb7e0554f9208d99ae403aa0ec87ae90fba
mickykun-ar <iiz10ninja@gmail.com>
2021-10-21 16:28:07 -0500

    core change: restart replies, doesnt react.

--- src/commands/core/restart.js
+++ src/commands/core/restart.js
@@ -11,25 +11,19 @@ module.exports = class CommandRestart extends Command {
         text: 'Attempts to restart the bot.',
         usage: '[reason]',
         details: '`[reason]` The reason for restarting the bot.'
-      },
-      args: [
-        {
-          type: 'string',
-          id: 'reason',
-          match: 'phrase'
-        }
-      ]
+      }
     });
   }
 
-  async exec (message, args) {
-    let restartReport = args.reason;
+  async exec (message) {
+    const args = message.content.split(/ +/g);
+    let restartReport = args.slice(1).join(' ');
     if (!restartReport) restartReport = 'Just refreshing... nothing serious. :3';
     const errChannel = this.client.channels.cache.find(val => val.id === process.env.BUG_CHANNEL);
-    await message.react('🔄');
+    await message.channel.send('🔄 Restarting...');
     const embed = new MessageEmbed()
       .setColor(process.env.COLOR_INFO)
-      .setTitle('🔁 Restart')
+      .setTitle('🔄 Restart')
       .setDescription(`\`\`\`js\n${restartReport}\`\`\``)
       .setTimestamp();
     await errChannel.send({ embeds: [embed] });
