0dba95c6797c175b258df6b4d54276610cec3e3d
mickykun-ar <iiz10ninja@gmail.com>
2021-12-21 01:41:34 -0600

    eval outputs a file for large results instead.

--- src/commands/core/eval.js
+++ src/commands/core/eval.js
@@ -52,8 +52,10 @@ module.exports = class CommandEval extends Command {
       let evaled = eval(code);
 
       if (typeof evaled !== 'string') {
-        evaled = require('util').inspect(evaled, { depth: 0, sorted: true, maxArrayLength: 5 });
+        evaled = require('util').inspect(evaled, { depth: 1, sorted: true, maxArrayLength: 5 });
       }
+
+      message.channel.sendTyping();
 
       const t2 = process.hrtime(t1);
       const end = (t2[0] * 1000000000 + t2[1]) / 1000000;
@@ -69,9 +71,27 @@ module.exports = class CommandEval extends Command {
       } else {
         const result = clean(evaled);
         if (result.length > 2000) {
-          const compactResult = result.match(/.{1,1960}/g);
-          for (const chunk of compactResult) {
-            await message.channel.send(`\`\`\`js\n// ✅ Evaluated in ${end} ms.\n${chunk}\`\`\``);
+          let tooLarge;
+          message.channel.send(':warning: `eval()` output is too large. Creating file...\n');
+          try {
+            const buffer = Buffer.from(`// ✅ Evaluated in ${end} ms.\n${result}`);
+            const file = new Discord.MessageAttachment(buffer, 'eval.txt');
+            if (!message.channel.permissionsFor(this.client.user.id).has(Discord.Permissions.FLAGS.ATTACH_FILES)) {
+              tooLarge = ':no_entry_sign: No permission to upload files in this channel.\n';
+              try {
+                message.author.send({ files: [file] });
+                tooLarge += ':mailbox_with_mail: Check your DMs, the logs, or the console for the output.\n';
+              } catch {
+                tooLarge += ':no_entry_sign: You are not accepting DMs. Check the console or logs for the output.\nickname';
+              }
+              return message.channel.send(tooLarge);
+            }
+            message.channel.send({ content: tooLarge, files: [file] });
+          } catch {
+            tooLarge = '\n:x: Failed to make a file for the eval output. Check the logs or the console for the output.\n';
+            message.channel.send(tooLarge);
+          } finally {
+            this.client.logger.info('Took %s ms. to complete.\n%s', end, clean(evaled));
           }
         } else {
           return message.channel.send(`\`\`\`js\n// ✅ Evaluated in ${end} ms.\n${result}\`\`\``);
