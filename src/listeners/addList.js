ef8de89b6c8654b1b32a2e5ea460a0229581d71d
200percentmicky <iiz10ninja@gmail.com>
2022-02-18 17:40:59 -0600

    queue limits now inherit addlist embed

--- src/listeners/addList.js
+++ src/listeners/addList.js
@@ -14,6 +14,20 @@ module.exports = class ListenerAddList extends Listener {
     const guild = channel.guild;
     const member = channel.guild.members.cache.get(queue.songs[queue.songs.length - 1].user.id);
 
+    const embed = new MessageEmbed()
+      .setColor(guild.me.displayColor !== 0 ? guild.me.displayColor : null)
+      .setAuthor({
+        name: `Playlist added to queue - ${member.voice.channel.name}`,
+        iconURL: guild.iconURL({ dynamic: true })
+      })
+      .setTitle(playlist.name)
+      .setURL(playlist.url)
+      .setThumbnail(playlist.thumbnail)
+      .setFooter({
+        text: playlist.user.tag,
+        iconURL: playlist.user.avatarURL({ dynamic: true })
+      });
+
     // Cut some or many entries if maxQueueLimit is in place.
     const djRole = this.client.settings.get(channel.guild.id, 'djRole');
     const dj = member.roles.cache.has(djRole) || channel.permissionsFor(member.user.id).has(Permissions.FLAGS.MANAGE_CHANNELS);
@@ -27,24 +41,12 @@ module.exports = class ListenerAddList extends Listener {
         // in the array, so we don't need to subtract the number to get
         // the correct element.
         queue.songs.splice(allowedLimit, playlist.songs.length - maxQueueLimit);
-        return this.client.ui.say(channel, 'ok', `${playlist.user} added the playlist **${playlist.name}** with **${playlist.songs.length}** entr${playlist.songs.length === 1 ? 'y' : 'ies'}. However, only the first ${maxQueueLimit > 1 ? `${maxQueueLimit} entries` : 'entry'} was added due to queue limits on this server.`);
+        embed.addField(':warning: Not everything was added!', `Due to limits set on this server, only the first ${maxQueueLimit > 1 ? `**${maxQueueLimit}** entries` : 'entry'}** out of **${playlist.songs.length}** was added to the queue.`);
+        return channel.send({ embeds: [embed] });
       }
+    } else {
+      embed.addField('🔢 Number of entries', `${playlist.songs.length}`);
+      channel.send({ embeds: [embed] });
     }
-
-    const embed = new MessageEmbed()
-      .setColor(guild.me.displayColor !== 0 ? guild.me.displayColor : null)
-      .setAuthor({
-        name: `Playlist added to queue - ${member.voice.channel.name}`,
-        iconURL: guild.iconURL({ dynamic: true })
-      })
-      .setTitle(playlist.name)
-      .setURL(playlist.url)
-      .addField('🔢 Number of entries', `${playlist.songs.length}`)
-      .setThumbnail(playlist.thumbnail)
-      .setFooter({
-        text: playlist.user.tag,
-        iconURL: playlist.user.avatarURL({ dynamic: true })
-      });
-    channel.send({ embeds: [embed] });
   }
 };
