cf2972813190880663a024c1e0729fa7aab3aa28
200percentmicky <iiz10ninja@gmail.com>
2022-03-01 03:25:05 -0600

    Removed blocks in ready event

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -9,21 +9,6 @@ module.exports = class SurferReady extends Listener {
   }
 
   async exec () {
-    // This is specific for my bot. It'll have no affect on your own.
-    if (process.env.IS_POKI === 'true') {
-      this.client.commands.remove('invite');
-    }
-
-    // Disbales the eval command is USE_EVAL is not 'true'.
-    if (process.env.USE_EVAL !== 'true') {
-      this.client.commands.remove('eval');
-    }
-
-    // Disables the shell command if USE_SHELL is not 'true'.
-    if (process.env.USE_SHELL !== 'true') {
-      this.client.commands.remove('shell');
-    }
-
     this.client.logger.info('Logged in as %s (%d)', this.client.user.tag, this.client.user.id);
     this.client.logger.info('[Ready!<3♪] Let\'s party!!');
   }
