413b32a622b5a53ba878958c4bfcf56cbceaefb5
200percentmicky <iiz10ninja@gmail.com>
2022-04-07 23:37:33 -0500

    errors contain links to repo and support server

--- src/listeners/clientCommandError.js
+++ src/listeners/clientCommandError.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const { MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = class ListenerClientCommandError extends Listener {
     constructor () {
@@ -9,8 +10,28 @@ module.exports = class ListenerClientCommandError extends Listener {
     }
 
     async exec (error, message, command) {
-        const guru = `💢 **Bruh Moment**\nSomething bad happened. Please report this to the developer.\n\`\`\`js\n${error.stack}\`\`\``;
-        message.reply({ content: guru, allowedMentions: { repliedUser: true } });
+        let guru = '💢 **Bruh Moment**\nSomething bad happened. Please report this to the developer.';
+
+        if (message.guild.channels.cache.get(process.env.BUG_CHANNEL)) {
+            guru += ' The owner of this application has also received a full error report.\n';
+        }
+
+        guru += `\`\`\`js\n${error.name}: ${error.message}\`\`\``;
+
+        const urlGithub = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://github.com/200percentmicky/ChadMusic')
+            .setLabel('GitHub Repo');
+
+        const support = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://discord.com/invite/qQuJ9YQ')
+            .setLabel('Support Server');
+
+        const actionRow = new MessageActionRow()
+            .addComponents([urlGithub, support]);
+
+        message.reply({ content: guru, components: [actionRow], allowedMentions: { repliedUser: true } });
         this.client.ui.recordError(this.client, command, '❌ Command Error', error.stack);
         this.client.logger.error('[Client] Error in command "%s": %s', command, error.stack);
     }
--- src/listeners/creatorCommandError.js
+++ src/listeners/creatorCommandError.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const { MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = class ListenerCreatorCommandError extends Listener {
     constructor () {
@@ -9,8 +10,32 @@ module.exports = class ListenerCreatorCommandError extends Listener {
     }
 
     async exec (command, err, ctx) {
-        const guru = `💢 **Bruh Moment**\nSomething bad happened. Please report this to the developer.\n\`\`\`js\n${err.stack}\`\`\``;
-        ctx.send(guru, { ephemeral: true });
+        await ctx.defer({ ephemeral: true });
+        let guru = '💢 **Bruh Moment**\nSomething bad happened. Please report this to the developer.';
+
+        const guild = this.client.guilds.cache.get(ctx.guildID);
+        if (guild.channels.cache.get(process.env.BUG_CHANNEL)) {
+            guru += ' The owner of this application has also received a full error report.\n';
+        }
+
+        guru += `\`\`\`js\n${err.name}: ${err.message}\`\`\``;
+
+        const urlGithub = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://github.com/200percentmicky/ChadMusic')
+            .setLabel('GitHub Repo');
+
+        const support = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://discord.com/invite/qQuJ9YQ')
+            .setLabel('Support Server');
+
+        const actionRow = new MessageActionRow()
+            .addComponents([urlGithub, support]);
+
+        await ctx.send(guru, {
+            components: [actionRow]
+        });
         this.client.ui.recordError(this.client, command.commandName, '❌ Command Error', err.stack);
         this.client.logger.error('[SlashCreator] Error in command "%s": %s', command.commandName, err.stack);
     }
