75ea848a0632095d8ed17d20eed6f9599c1623e0
200percentmicky <iiz10ninja@gmail.com>
2022-05-05 17:28:53 -0500

    Changing mentionprefix

--- src/listeners/mentionPrefix.js
+++ src/listeners/mentionPrefix.js
@@ -1,5 +1,4 @@
 const { Listener } = require('discord-akairo');
-const { Permissions } = require('discord.js');
 
 module.exports = class ListenerMentionPrefix extends Listener {
     constructor () {
@@ -10,13 +9,8 @@ module.exports = class ListenerMentionPrefix extends Listener {
     }
 
     async exec (message) {
-        const prefix = this.client.settings.get(message.guild.id, 'prefix', process.env.PREFIX);
-        let canChange;
-        if (message.channel.permissionsFor(message.member?.user?.id).has(Permissions.FLAGS.MANAGE_GUILD)) {
-            canChange = ` | You can run \`${prefix}musicprefix\` to change this.`;
-        }
-        if (message.content === `<@!${this.client?.user?.id}>`) {
-            return message.channel.send(`${process.env.EMOJI_MUSIC} My prefix for music commands in **${message.guild.name}** is \`${prefix}\`.${canChange}`);
+        if (message.content === `<@${this.client.user.id}>`) {
+            return message.channel.send(`${process.env.EMOJI_MUSIC} My prefix for music commands is \`${process.env.PREFIX}\`. You can also use the applications's Slash Commands, or use the bot's mention as a prefix.`);
         }
     }
 };
