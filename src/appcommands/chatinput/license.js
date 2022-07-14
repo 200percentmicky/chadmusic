d602d72183c5abe3d7e51a3c8654bbb2075da5e6
Micky D <iiz10ninja@gmail.com>
2022-07-14 01:41:09 -0500

    adding license command

---
+++ src/appcommands/chatinput/license.js
@@ -0,0 +1,71 @@
+/**
+ *  Micky-bot
+ *  Copyright (C) 2022  Micky D. | @200percentmicky | Micky-kun#3836
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { SlashCommand } = require('slash-create');
+const { stripIndents } = require('common-tags');
+const { MessageButton, MessageActionRow } = require('discord.js');
+
+class CommandLicense extends SlashCommand {
+    constructor (creator) {
+        super(creator, {
+            name: 'license',
+            description: 'View this program\'s license.'
+        });
+
+        this.filePath = __filename;
+    }
+
+    async run (ctx) {
+        await ctx.send(true);
+
+        const urlGithub = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://github.com/200percentmicky/mickybot')
+            .setLabel('GitHub');
+
+        const support = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://discord.com/invite/qQuJ9YQ')
+            .setLabel('Support Server');
+
+        const actionRow = new MessageActionRow()
+            .addComponents([urlGithub, support]);
+
+        return ctx.send({
+            content: stripIndents`
+            Micky-bot is licensed under the GNU General Public License version 3.
+
+            This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            
+            This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+            
+            You should have received a copy of the GNU General Public License
+            along with this program.  If not, see <https://www.gnu.org/licenses/>.
+            `,
+            components: [actionRow]
+        });
+    }
+}
+
+module.exports = CommandLicense;
---
+++ src/commands/core/license.js
@@ -0,0 +1,68 @@
+/**
+ *  Micky-bot
+ *  Copyright (C) 2022  Micky D. | @200percentmicky | Micky-kun#3836
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { stripIndents } = require('common-tags');
+const { Command } = require('discord-akairo');
+const { MessageButton, MessageActionRow } = require('discord.js');
+
+module.exports = class LicenseCommand extends Command {
+    constructor () {
+        super('license', {
+            aliases: ['license'],
+            description: {
+                text: 'View this program\'s license.'
+            },
+            category: '💻 Core'
+        });
+    }
+
+    async exec (message) {
+        const urlGithub = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://github.com/200percentmicky/mickybot')
+            .setLabel('GitHub');
+
+        const support = new MessageButton()
+            .setStyle('LINK')
+            .setURL('https://discord.com/invite/qQuJ9YQ')
+            .setLabel('Support Server');
+
+        const actionRow = new MessageActionRow()
+            .addComponents([urlGithub, support]);
+
+        return message.author.send({
+            content: stripIndents`
+            Micky-bot is licensed under the GNU General Public License version 3.
+
+            This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            
+            This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+            
+            You should have received a copy of the GNU General Public License
+            along with this program.  If not, see <https://www.gnu.org/licenses/>.
+            `,
+            components: [actionRow]
+        });
+    }
+};
