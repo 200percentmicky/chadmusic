b2c76558a33534a57dd25dc2613876dcd0b3be72
200percentmicky <iiz10ninja@gmail.com>
2023-02-09 22:58:35 -0600

    add function to handle slash commands to normal commands

---
+++ src/modules/handleCommand.js
@@ -0,0 +1,36 @@
+/**
+ *  ChadMusic - The Chad Music Bot
+ *  Copyright (C) 2022  Micky D. | @200percentmicky | Micky-kun#3836
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { Command, Argument } = require('discord-akairo'); // eslint-disable-line no-unused-vars
+const { CommandContext } = require('slash-create'); // eslint-disable-line no-unused-vars
+
+/**
+ * Used mainly for slash commands to run message based commands
+ * attached to the Client.
+ *
+ * This function relies on slash-create and Discord Akairo.
+ * @param {CommandContext} string CommandContext
+ * @param {Command|string} commandName The command attached to Akairo.
+ * @param {Argument|Object} args Arguments passed to a command.
+ */
+async function handleCommand (ctx, commandName, args) {
+    const command = this.client.commands.findCommand(commandName);
+    return this.client.commands.runCommand(ctx, command, args);
+}
+
+module.exports = { handleCommand };
