c5b89296976f5186a0e17ce80ec5293e21c1b3f6
200percentmicky <iiz10ninja@gmail.com>
2023-02-09 23:07:00 -0600

    add client param

--- src/modules/handleCommand.js
+++ src/modules/handleCommand.js
@@ -16,7 +16,8 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const { Command, Argument } = require('discord-akairo'); // eslint-disable-line no-unused-vars
+const { Command, Argument, AkairoClient } = require('discord-akairo'); // eslint-disable-line no-unused-vars
+const { Client } = require('discord.js'); // eslint-disable-line no-unused-vars
 const { CommandContext } = require('slash-create'); // eslint-disable-line no-unused-vars
 
 /**
@@ -24,13 +25,14 @@ const { CommandContext } = require('slash-create'); // eslint-disable-line no-un
  * attached to the Client.
  *
  * This function relies on slash-create and Discord Akairo.
+ * @param {Client|AkairoClient} client Discord.JS Client attached to Akairo Client.
  * @param {CommandContext} string CommandContext
  * @param {Command|string} commandName The command attached to Akairo.
  * @param {Argument|Object} args Arguments passed to a command.
  */
-async function handleCommand (ctx, commandName, args) {
-    const command = this.client.commands.findCommand(commandName);
-    return this.client.commands.runCommand(ctx, command, args);
+async function handleCommand (client, ctx, commandName, args) {
+    const command = client.commands.findCommand(commandName);
+    return client.commands.runCommand(ctx, command, args);
 }
 
 module.exports = { handleCommand };
