debe4614454b69c8eaf0c7e02ca7616290560ce0
200percentmicky <iiz10ninja@gmail.com>
2023-04-26 00:34:01 -0500

    chore: modify license template in all files

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,20 +1,18 @@
-/**
- *  ChadMusic - The Chad Music Bot
- *  Copyright (C) 2023  Micky D. | @200percentmicky | Micky-kun#3836
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+/// ChadMusic - The Chad Music Bot
+/// Copyright (C) 2023  Micky D. | @200percentmicky | Micky-kun#3836
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const { Command } = require('discord-akairo');
 const { PermissionsBitField, Message } = require('discord.js');
