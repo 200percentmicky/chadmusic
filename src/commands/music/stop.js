c94d776c6b89fbc1d15a79313c5ddbc64831a893
200percentmicky <iiz10ninja@gmail.com>
2023-04-29 02:20:04 -0500

    fix: leaveOnStop wasnt being respected

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -55,7 +55,6 @@ module.exports = class CommandStop extends Command {
 
         if (vc.members.size <= 2 || dj) {
             this.client.player.stop(message);
-            this.client.vc.leave(message.guild);
             return this.client.ui.custom(message, '⏹', process.env.COLOR_INFO, 'Stopped the player and cleared the queue.');
         } else {
             return this.client.ui.sendPrompt(message, 'NOT_ALONE');
