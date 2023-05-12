8119f5a68f6dba5275003dee4d3e9d570f52d187
200percentmicky <iiz10ninja@gmail.com>
2023-05-11 19:50:05 -0500

    feat: Added new command: globalsettings

---
+++ src/commands/settings/globalsettings.js
@@ -0,0 +1,72 @@
+/// ChadMusic - The Chad Music Bot
+/// Copyright (C) 2023  Micky D. | @200percentmicky | Micky-kun#3836
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const { stripIndents } = require('common-tags');
+const { Command } = require('discord-akairo');
+const { EmbedBuilder } = require('discord.js');
+const { version } = require('../../../package.json');
+
+module.exports = class CommandGlobalSettings extends Command {
+    constructor () {
+        super('globalsettings', {
+            aliases: ['globalsettings'],
+            category: '⚙ Settings',
+            description: {
+                text: 'Shows you the bot\'s current global settings.'
+            },
+            ownerOnly: true
+        });
+    }
+
+    async exec (message) {
+        const settings = this.client.settings;
+
+        await settings.ensure('global', this.client.defaultGlobalSettings);
+
+        // Global Settings
+        const emitNewSongOnly = settings.get('global', 'emitNewSongOnly'); // Show New Song Only
+        const emptyCooldown = settings.get('global', 'emptyCooldown'); // Empty Cooldown
+        const leaveOnEmpty = settings.get('global', 'leaveOnEmpty'); // Leave on Empty
+        const leaveOnFinish = settings.get('global', 'leaveOnFinish'); // Leave on Finish
+        const leaveOnStop = settings.get('global', 'leaveOnStop'); // Leave on Stop
+        const streamType = settings.get('global', 'streamType'); // Audio Encoder
+
+        const encoderType = {
+            0: 'Opus',
+            1: 'RAW'
+        };
+
+        const embed = new EmbedBuilder()
+            .setColor(message.guild.members.me.displayColor !== 0 ? message.guild.members.me.displayColor : null)
+            .setAuthor({
+                name: `ChadMusic v${version}`,
+                iconURL: 'https://media.discordapp.net/attachments/375453081631981568/808626634210410506/deejaytreefiddy.png'
+            })
+            .setTitle('🌐 Global Settings')
+            .setDescription(stripIndents`
+                **Audio Encoder:** ${encoderType[streamType]}
+                **Empty Cooldown:** ${parseInt(emptyCooldown)} seconds
+                **Leave on Empty:** ${leaveOnEmpty === true ? 'On' : 'Off'}
+                **Leave on Finish:** ${leaveOnFinish === true ? 'On' : 'Off'}
+                **Leave on Stop:** ${leaveOnStop === true ? 'On' : 'Off'}
+                **Show New Song Only:** ${emitNewSongOnly === true ? 'On' : 'Off'}
+                `
+            )
+            .setTimestamp();
+
+        return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    }
+};
