0aabb364466dcf29ab1725345e3d968296ba1c8b
200percentmicky <iiz10ninja@gmail.com>
2023-11-05 23:05:45 -0600

    fix: url is now fetched from track object

--- src/commands/playlist/playlist-add.js
+++ src/commands/playlist/playlist-add.js
@@ -82,11 +82,11 @@ module.exports = class CommandPlaylistAdd extends Command {
                 let track;
 
                 try {
-                    track = await ytdl.getInfo(args.track?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url);
+                    track = await ytdl.getInfo(args.track?.href?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url);
                 } catch {
                     for (const p of this.client.player.extractorPlugins) {
-                        if (p.validate(args.track?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url)) {
-                            track = await p.resolve(args.track?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url, {
+                        if (p.validate(args.track?.href?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url)) {
+                            track = await p.resolve(args.track?.href?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url, {
                                 member: message.member
                             });
                         }
@@ -99,7 +99,7 @@ module.exports = class CommandPlaylistAdd extends Command {
                     date_added: Math.floor(Date.now() / 1000)
                 };
 
-                if (!this.client.utils.hasURL(args.track?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url) || !track) {
+                if (!this.client.utils.hasURL(args.track?.href?.replace(/(^\<+|\>+$)/g, '') ?? player.songs[0].url) || !track) {
                     return this.client.ui.reply(message, 'error', 'The track must be a URL.');
                 }
 
