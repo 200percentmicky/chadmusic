990c1444ccf45f8b001872a9185f4614a84d121a
200percentmicky <iiz10ninja@gmail.com>
2023-12-13 21:05:03 -0600

    add custom error class

---
+++ src/modules/CMError.js
@@ -0,0 +1,35 @@
+/// ChadMusic - The Chad Music Bot
+/// Copyright (C) 2023  Micky D. | @200percentmicky | Micky-kun#3836
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Error class for the bot to use.
+ *
+ * @param {string} [type]
+ * @param {any} [metadata]
+ * @param {string} [message]
+ */
+
+class CMError extends Error {
+    constructor (type, metadata, message) {
+        super();
+        this.name = `CMError${type ? ` [${type}]` : ''}`;
+        this.message = message ?? 'Failed successfully.'; // lol
+        this.type = type;
+        this.metadata = metadata;
+    }
+}
+
+module.exports = CMError;
