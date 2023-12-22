0822cf9140bd3772aa045da014b6b27728ea9d00
Micky D. <iiz10ninja@gmail.com>
2023-12-22 15:05:03 -0600

    updating prompts

--- src/listeners/commandBlocked.js
+++ src/listeners/commandBlocked.js
@@ -26,8 +26,8 @@ module.exports = class CommandBlockedListener extends Listener {
     }
 
     async exec (message, command, reason) {
-        if (reason === 'owner') return this.client.ui.reply(message, 'no', 'That command can only be used by the bot owner.');
-        if (reason === 'guild') return this.client.ui.reply(message, 'error', 'That command must be used in a server.');
-        if (reason === ChannelType.DM) return this.client.ui.reply(message, 'error', 'That command must be used in a Direct Message.');
+        if (reason === 'owner') return this.client.ui.reply(message, 'no', 'This command can only be used by the bot owner.');
+        if (reason === 'guild') return this.client.ui.reply(message, 'no', 'This command cannot be used in Direct Messages.');
+        if (reason === ChannelType.DM) return this.client.ui.reply(message, 'no', 'This command cannot be used in servers.');
     }
 };
