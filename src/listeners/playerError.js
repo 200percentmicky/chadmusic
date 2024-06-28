3ffcde1e2dde8834cc277e745b70e0c7282e654a
200percentmicky <iiz10ninja@gmail.com>
2024-06-28 09:54:33 -0500

    update playerError to use new param change

--- src/listeners/playerError.js
+++ src/listeners/playerError.js
@@ -25,7 +25,7 @@ module.exports = class ListenerPlayerError extends Listener {
         });
     }
 
-    async exec (channel, error) {
+    async exec (error, queue, song) {
         const errsplit = error.message.split(/ +/g);
 
         const knownErrors = {
@@ -47,7 +47,7 @@ module.exports = class ListenerPlayerError extends Listener {
             .setColor(process.env.COLOR_ERROR)
             .setTitle(`${process.env.EMOJI_ERROR} Player Error`);
 
-        const linkPerms = channel.permissionsFor(this.client.user.id).has(PermissionsBitField.Flags.EmbedLinks);
+        const linkPerms = queue.textChannel.permissionsFor(this.client.user.id).has(PermissionsBitField.Flags.EmbedLinks);
 
         // Iterates through the split error message. If it finds a match, it will
         // return an easier to understand message. Otherwise, return a more detailed
@@ -60,19 +60,16 @@ module.exports = class ListenerPlayerError extends Listener {
         }
 
         embed.setDescription(`${formattedError ? `${formattedError}\n` : ''}${error}`);
+        embed.addFields([
+            {
+                name: 'Track',
+                value: `**[${song.name}](${song.url})**`
+            }
+        ]);
 
-        channel.send(linkPerms
+        queue.textChannel.send(linkPerms
             ? { embeds: [embed] }
-            : { content: `${process.env.EMOJI_ERROR} **Player Error**\n${formattedError}\n${error}` }
+            : { content: `${process.env.EMOJI_ERROR} **Player Error**\n**[${song.name}](${song.url})**\n${formattedError}\n${error}` }
         );
-
-    /*
-    if (errsplit.includes('extract') || errsplit.includes('Unsupported')) return this.client.ui.reply(message, 'error', 'Not a supported URL or the URL is invalid.', 'Player Error')
-    if (errsplit.includes('403')) return this.client.ui.reply(message, 'error', 'URL returned HTTP 403 (Forbidden)')
-    if (errsplit.includes('416')) return this.client.ui.reply(message, 'error', 'Received `Status code: 416 [Range Not Satisfiable]`. This is a weird response. Please try again.', 'Player Error')
-    if (errsplit.includes('Cookie')) return this.client.ui.reply(message, 'error', 'Cookie header used in the request has expired. The bot owner has been notified about this.', 'Player Error')
-    if (errsplit.includes('format')) return this.client.ui.reply(message, 'error', 'Cannot find a format of the video to play. The owner of the video has disabled playback on other websites, or the video is unavailable.', 'Player Error')
-    if (error.name.includes('Error [VOICE_CONNECTION_TIMEOUT]')) return this.client.ui.reply(message, 'error', 'The connection was not established within 15 seconds. Please try again later.', 'Voice Connection Timeout')
-    */
     }
 };
