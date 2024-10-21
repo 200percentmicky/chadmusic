e913c9779fd9962ab2a390e1c810e43531a1d7b4
200percentmicky <iiz10ninja@gmail.com>
2024-10-21 17:46:31 -0500

    update debug command

--- src/appcommands/chatinput/core.js
+++ src/appcommands/chatinput/core.js
@@ -553,41 +553,56 @@ class CommandCore extends SlashCommand {
             if (!this.client.owner) await this.client.application.fetch();
             const owner = this.client.owner instanceof Discord.Team ? `${this.client.owner?.name}` : `${this.client.owner?.tag.replace(/#0{1,1}$/, '')} (${this.client.owner?.id})`;
 
+            let kernel;
+            let release;
+            switch (osSi.platform) {
+            case 'Windows':
+                kernel = `Windows NT ${osSi.kernel} Service Pack ${osSi.servicepack}`;
+                release = `${osSi.release} (Build ${osSi.build})`;
+                break;
+            case 'darwin':
+                kernel = `Darwin ${osSi.kernel} ${osSi.build}`;
+                release = `${osSi.codename} ${osSi.release}`;
+                break;
+            case 'linux':
+                kernel = `Linux ${osSi.kernel} ${osSi.build}`;
+                release = `${osSi.release} ${osSi.codename}`;
+                break;
+            }
+
             const data = stripIndents`
-            **ChadMusic - The Chad Music Bot!**
-            Bot version ${this.client.version}
+            === ChadMusic ===
+            Version :: ${this.client.version}
             
-                      Client: ${this.client.user.tag.replace(/#0{1,1}$/, '')} (ID: ${this.client.user.id})
-                       Owner: ${owner}
-                     Node.js: ${process.version}
-                  Discord.js: ${Discord.version}
-            Akairo Framework: ${akairo.version}
-                  DisTube.js: ${this.client.player.version}
-                slash-create: ${sc.version}
-           Voice Connections: ${this.client.vc.voices.collection.size}
-                      Uptime: ${prettyMs(this.client.uptime, { verbose: true })}
-                  Cluster ID: ${this.client.cluster?.id ?? 'N/A'}
-                    Shard ID: ${guild.shardId ?? 'N/A'}
+            Client :: ${this.client.user.tag.replace(/#0{1,1}$/, '')} (ID: ${this.client.user.id})
+            Owner :: ${owner}
+            Node.js :: ${process.version}
+            Discord.js :: ${Discord.version}
+            DisTube :: ${player.version}
+            slash-create :: ${sc.version}
+            Voice Connections :: ${this.client.vc.voices.collection.size}
+            Uptime :: ${prettyms(this.client.uptime, { verbose: true })}
+            Cluster ID :: ${this.client.cluster?.id ?? 'N/A'}
+            Shard ID :: ${guild.shardId ?? 'N/A'}
         
-            __**System**__
-                      CPU: ${os.cpus()[0].model}
-                CPU Speed: ${os.cpus()[0].speed} MHz.
-             Memory Total: ${prettyBytes(memory.total)}
-              Memory Used: ${prettyBytes(memory.used)}
-              Memory Free: ${prettyBytes(memory.free)}
-               Swap Total: ${prettyBytes(memory.swaptotal)}
-                Swap Used: ${prettyBytes(memory.swapused)}
-                Swap Free: ${prettyBytes(memory.swapfree)}
-                 Platform: ${osSi.platform}
-               OS Version: ${osSi.distro} ${osSi.release} ${osSi.platform === 'darwin' ? osSi.codename : ''}
-                   Kernel: ${osSi.kernel}
-            Architechture: ${osSi.arch}
-                     User: ${user.username}
-                    Shell: ${user.shell}
-              ${osSi.platform === 'win32' ? `Service Pack: ${osSi.servicepack}` : ''}
+            System
+            ------
+            CPU :: ${os.cpus()[0].model}
+            CPU Speed :: ${os.cpus()[0].speed} MHz.
+            Memory Total :: ${prettyBytes(memory.total)}
+            Memory Used :: ${prettyBytes(memory.used)}
+            Memory Free :: ${prettyBytes(memory.free)}
+            Swap Total :: ${prettyBytes(memory.swaptotal)}
+            Swap Used :: ${prettyBytes(memory.swapused)}
+            Swap Free :: ${prettyBytes(memory.swapfree)}
+            OS Version :: ${osSi.distro} ${release}
+            Kernel :: ${kernel}
+            Architechture :: ${osSi.arch}
+            User :: ${user.username}
+            Shell :: ${user.shell}
             `;
 
-            await ctx.send(data);
+            return ctx.send({ content: `\`\`\`asciidoc\n${data}\`\`\`` });
         }
         }
     }
